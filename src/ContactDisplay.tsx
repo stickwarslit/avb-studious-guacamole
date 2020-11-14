ab72dda30a1e2ac0c991b0d4ef6c4fdb71fc74af
Mark Emmanuel Del Rosario <MarkESDR@gmail.com>
2020-11-14 14:44:17 -0500

    Add contact input fields

--- src/ContactDisplay.tsx
+++ src/ContactDisplay.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Contact } from './data/contacts'
 
 interface Props {
@@ -6,12 +6,26 @@ interface Props {
 }
 
 export default function ContactsDisplay({contact}: Props) {
+  const [firstName, setFirstName] = useState(contact.firstName)
+  const [lastName, setLastName] = useState(contact.lastName)
+
+  // Change display firstName and lastName when contact changes
+  useEffect(() => {
+    setFirstName(contact.firstName)
+    setLastName(contact.lastName)
+  }, [contact])
+
+  const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)
+  const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)
+
   return (
+    <article className="contact-display">
+      <form>
+        <label>First Name: <input type="text" value={firstName} onChange={handleFirstNameChange}/></label>
+        <label>Last Name: <input type="text" value={lastName} onChange={handleLastNameChange}/></label>
+      </form>
     
-    <div>
-      {contact.id}
-      <span>{contact.firstName} {contact.lastName}</span>
-    </div>
+    </article>
   )
 
 }
