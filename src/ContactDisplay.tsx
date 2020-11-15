83a87de1ea21b819a3825bd5cdad50bb2672902f
Mark Emmanuel Del Rosario <MarkESDR@gmail.com>
2020-11-15 18:32:14 -0500

    Use simpler email regex

--- src/ContactDisplay.tsx
+++ src/ContactDisplay.tsx
@@ -3,8 +3,8 @@ import { Contact  } from './data/contacts'
 import { useContact } from './context/Contact'
 
 
-// Copied from http://emailregex.com/
-const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+// Copied from https://ihateregex.io/expr/email/
+const EMAIL_REGEX = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
 
 const isValidEmail = (email: string): boolean => {
   return EMAIL_REGEX.test(email)
@@ -46,6 +46,7 @@ export default function ContactsDisplay({contact, onDelete}: Props) {
       setEmails(emails.concat(newEmail))
       setNewEmail("")
       setAddingEmail(false)
+      setMessage("")
     } else {
       setMessage("Invalid email address")
     }
