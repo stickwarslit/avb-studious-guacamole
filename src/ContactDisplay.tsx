94991073cc1e78c6c71d29039b12e628fd77d494
Mark Emmanuel Del Rosario <MarkESDR@gmail.com>
2020-11-15 18:14:21 -0500

    Allow adding of emails

--- src/ContactDisplay.tsx
+++ src/ContactDisplay.tsx
@@ -6,6 +6,14 @@ interface Props {
   contact: Contact
 }
 
+// Copied from http://emailregex.com/
+const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+const isValidEmail = (email: string): boolean => {
+  return EMAIL_REGEX.test(email)
+}
+
+
 export default function ContactsDisplay({contact}: Props) {
   const { updateContact } = useContact()
 
@@ -13,6 +21,8 @@ export default function ContactsDisplay({contact}: Props) {
   const [lastName, setLastName] = useState(contact.lastName)
   const [emails, setEmails] = useState(contact.emails)
 
+  const [isAddingEmail, setAddingEmail] = useState(false)
+  const [newEmail, setNewEmail] = useState("")
   const [message, setMessage] = useState("")
 
   // Change contact display info when contact changes
@@ -26,13 +36,25 @@ export default function ContactsDisplay({contact}: Props) {
 
   const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)
   const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)
+  const handleNewEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => setNewEmail(e.target.value)
+
+  const completeNewEmail = () => {
+    if (isValidEmail(newEmail)) {
+      setEmails(emails.concat(newEmail))
+      setNewEmail("")
+      setAddingEmail(false)
+    } else {
+      setMessage("Invalid email address")
+    }
+  }
 
   const handleSave = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation()
     const updatedContact: Contact = {
       ...contact,
       firstName,
-      lastName
+      lastName,
+      emails
     }
 
     await updateContact(updatedContact)
@@ -43,14 +65,30 @@ export default function ContactsDisplay({contact}: Props) {
 
   return (
     <article className="contact-display">
-      <label>First Name: <input type="text" value={firstName} onChange={handleFirstNameChange}/></label>
-      <label>Last Name: <input type="text" value={lastName} onChange={handleLastNameChange}/></label>
+      <label>First Name: </label>
+      <input type="text" value={firstName} onChange={handleFirstNameChange}/>
+      <label>Last Name: </label>
+      <input type="text" value={lastName} onChange={handleLastNameChange}/>
 
       <label>Emails</label>
       <ul>
-        {
-          emails.map(email => <li key="email">{email}</li>)
+        { emails.map(email => 
+          <li key={email}>{email}</li>
+        )}
+        <li>
+        { isAddingEmail
+            ? <>
+                <label>New Email: </label> 
+                <input type="text" value={newEmail} onChange={handleNewEmailChange} />
+                <button onClick={completeNewEmail}>Complete</button>
+              </>
+            : <>
+                <button onClick={() => setAddingEmail(true)}>
+                  add email
+                </button>
+              </>
         }
+        </li>
       </ul>
 
       <button onClick={handleSave}>Save</button>
